71cd736d88ace58742db7c2caffa2e9629a874ce
Shamsur-Rahaman <shamsurrahaman132@gmail.com>
2023-09-11 22:12:47 +0600

    initialization complete

---
+++ git_first.sh
@@ -0,0 +1,8 @@
+
+
+git init
+git add .
+git commit -m "initialization complete"
+git branch -M main
+git remote add origin https://github.com/Shamsur-Rahaman/fruit_brust.git
+git push -u origin main
